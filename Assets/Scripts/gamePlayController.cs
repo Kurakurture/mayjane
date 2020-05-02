fe5f2490b2550d801ca2ca2cdd5709e802872d23
DmitryUlitin <d.ulitin@pixonic.com>
2020-05-02 15:32:03 +0300

    FixedUpdate исправлен на Update

---
+++ Assets/Scripts/gamePlayController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamePlayController : MonoBehaviour
+{
+    public List<characterController> players;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        players.Add(GameObject.Find("character").GetComponent<characterController>());
+
+    }
+}
