56cf60bc892cdc11a59eda242bbc1688286b5a28
o.grachev <o.grachev@pixonic.ru>
2020-05-02 18:47:53 +0300

    генератор уровней

---
+++ Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour
+{
+    LevelManager lm;
+
+    private void Awake()
+    {
+        lm = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+    }
+
+    private void OnTriggerExit(Collider trigger)
+    {
+        characterController player = trigger.gameObject.GetComponent<characterController>();
+        if (trigger.gameObject.tag == "player" && player.active == true)
+        {
+            lm.pickLevel();
+            print("collision detected");
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }           
+    }
+}
