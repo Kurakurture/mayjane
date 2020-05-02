8fbcb58ceed25afe7efe072208d812ede6e6dcdf
DmitryUlitin <d.ulitin@pixonic.com>
2020-05-02 14:42:49 +0300

    Набросок персонажа

---
+++ Assets/Scripts/cameraPlayerLerp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraPlayerLerp : MonoBehaviour
+{
+    public characterController player;
+    public Transform selfTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(player == null){
+            player = GameObject.Find("character").GetComponent<characterController>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player == null){
+            player = GameObject.Find("character").GetComponent<characterController>();
+        }
+    }
+}
--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody charRigidbody;
+    public Collider charCollider;
+    public bool active = false;
+    public Vector3 foreceVector;
+    public float forceY;
+    public float randomForceY;
+    public float forceX;
+    public float randomForceX;
+    public Material[] charMaterials;
+    public ParticleSystem effect;
+
     void Start()
     {
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+        if(other.tag == "player")
+        active = true;
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    void FixedUpdate()
     {
-        
+        if (!active){
+            charRigidbody.useGravity = false;
+            effect.Play(false);
+            }
+        else{
+            charRigidbody.useGravity = true;
+            effect.Play(true);
+
+        }
+
+        if (Input.GetKeyDown("space"))
+            CharacterJump();
+    }
+
+    void CharacterJump()
+    {
+
+        Vector3 _force = foreceVector;
+        _force.y += forceY;
+        _force.x += forceX;
+
+
+        if (active)
+        {
+            charRigidbody.AddForce(_force);
+
+        }
+
+
     }
 }
